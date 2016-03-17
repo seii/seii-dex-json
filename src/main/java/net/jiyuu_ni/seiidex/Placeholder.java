fd192196d662532404ec18064b3776b9e7c9ac97
seii <seii@users.noreply.github.com>
2016-03-16 19:11:57 -0500

    Beginning of JSON code

--- src/main/java/net/jiyuu_ni/seiidex/Placeholder.java
+++ src/main/java/net/jiyuu_ni/seiidex/Placeholder.java
@@ -1,5 +1,31 @@
 package net.jiyuu_ni.seiidex;
 
-public class Placeholder {
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
 
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonParser;
+
+public class Placeholder {
+	
+	public static void main(String args[]) {
+		
+		try {
+			File tempFile = new File(Placeholder.class.getResource("").toURI());
+			
+			JsonFactory jsonFactory = new JsonFactory();
+			JsonParser jsonParser = jsonFactory.createParser(tempFile);
+		}
+		catch(URISyntaxException e) {
+			e.printStackTrace();
+		} catch (JsonParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
